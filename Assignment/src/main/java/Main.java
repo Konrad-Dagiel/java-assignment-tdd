65184528e4c8ddb968026e3b7f67004b0eff84ac
Konrad-Dagiel <kdawgy9@gmail.com>
2022-11-27 23:07:17 +0000

    deleted Main.java, as it is not required for submission

--- Assignment/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Colour c = new Colour(16777215);
-        System.out.println(Integer.toBinaryString(16777215));
-        System.out.println(c.getRGB());
-
-    }
-}
