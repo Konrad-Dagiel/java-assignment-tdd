afe0bec8e10f87d3432d961657134c89ea0aab5a
Konrad-Dagiel <kdawgy9@gmail.com>
2022-11-27 20:00:34 +0000

    added the test cases to throw IllegalArgumentExceptions on invalid inputs

--- Assignment/src/main/java/Colour.java
+++ Assignment/src/main/java/Colour.java
@@ -2,13 +2,15 @@ public class Colour {
     private final int red;
     private final int blue;
     private final int green;
-    public Colour(float r, float g, float b){
+    public Colour(float r, float g, float b) throws IllegalArgumentException {
+        if (!((r>=0 && r <= 1) && (g >= 0 && g <= 1) && (b>=0 && b <=1))){
+            throw new IllegalArgumentException("Values must be in range 0.0 - 1");
+        }
         red = (int) (r*255);
         green = (int) (g*255);
         blue = (int) (b*255);
     }
     public String getRGB(){
-        String RGBValue = red+", " + blue+", "+green;
-        return RGBValue;
+        return red+", " + blue+", "+green;
     }
 }
--- Assignment/src/test/java/ColourTest.java
+++ Assignment/src/test/java/ColourTest.java
@@ -3,9 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class ColourTest {
     @Test
-    void createWhiteFromFloats(){
+    void onesShouldCreateWhite() throws IllegalArgumentException {
         Colour colour = new Colour(1.0f, 1.0f, 1.0f);
         Assertions.assertEquals("255, 255, 255", colour.getRGB());
+    }
+    @Test
+    void inputsGreaterThanOneShouldThrowIllegalArgumentException(){
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> new Colour(1.1f, 1.1f, 1.1f)
+        );
+    }
+    @Test
+    void inputsLessThanZeroShouldThrowIllegalArgumentException(){
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> new Colour(-0.1f, -0.1f, -0.1f)
+        );
     }
 
 }
